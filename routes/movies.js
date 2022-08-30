36d6f5f27d02573122faf4d843e4b091f1e12e7b
Alina Zolotavina <abekmetova@yandex.ru>
2022-08-30 19:06:54 +0400

    fix: delete movie route

--- routes/movies.js
+++ routes/movies.js
@@ -11,20 +11,25 @@ router.get('/', getMovies);
 
 router.post('/', celebrate({
   body: Joi.object().keys({
-    country: Joi.string().required().min(1).max(100),
-    director: Joi.string().required().min(1).max(100),
+    country: Joi.string().required().min(1).max(50),
+    director: Joi.string().required().min(2).max(50),
     duration: Joi.number().required(),
-    year: Joi.string().required().min(2).max(4),
-    description: Joi.string().required().min(1).max(5000),
+    year: Joi.string().required().min(4).max(4),
+    description: Joi.string().required().min(10).max(1000),
     image: Joi.string().required().custom(validateUrl),
     trailerLink: Joi.string().required().custom(validateUrl),
     thumbnail: Joi.string().required().custom(validateUrl),
     movieId: Joi.number().required(),
-    nameRU: Joi.string().required().min(1).max(100),
-    nameEN: Joi.string().required().min(1).max(100),
+    nameRU: Joi.string().required().min(1).max(50),
+    nameEN: Joi.string().required().min(1).max(50),
   }),
 }), createMovie);
 
-router.delete('/_id', deleteMovie);
+router.delete('/:movieId', celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().required().alphanum().length(24)
+      .hex(),
+  }),
+}), deleteMovie);
 
 module.exports = router;
